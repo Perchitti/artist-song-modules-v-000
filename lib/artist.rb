6b0fb30b0f132b3bb73dd81bdc7505a5e0ffd8d6
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-05 17:09:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,8 +2,7 @@ require 'pry'
 
 class Artist
 
-  extend Memorable::ClassMethods
-  extend Findable:: ClassMethods
+  extend Memorable::ClassMethods, Findable::ClassMethods
   attr_accessor :name
   attr_reader :songs
 
